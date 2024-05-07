023e900142b1156f9caf51c3ba7187072d15ce31
Feras Jobeir <fjobeir@gmail.com>
2024-05-07 16:16:56 +0400

    final small cleanup

--- src/components/multi-selector/multi-selector.tsx
+++ src/components/multi-selector/multi-selector.tsx
@@ -111,16 +111,12 @@ const MultiSelector = <T extends BaseOption,>({
                     aria-expanded={showOptions}
                     disabled={options.length === 0}
                     onClick={() => setShowOptions((prevShowOptions) => !prevShowOptions)}>
-                    {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} className={`w-5 h-5 transition-all duration-300 ${!showOptions && 'rotate-180'} ${options.length ? 'text-gray-600' : 'text-gray-300'}`}>
-                        <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" stroke="currentColor" />
-                    </svg> */}
                     <svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className={`w-5 h-5 transition-all duration-300 ${!showOptions && 'rotate-180'} ${options.length ? 'text-gray-600' : 'text-gray-300'}`}>
                         <path fill="currentColor" d="m16.843 10.211c.108-.141.157-.3.157-.456 0-.389-.306-.755-.749-.755h-8.501c-.445 0-.75.367-.75.755 0 .157.05.316.159.457 1.203 1.554 3.252 4.199 4.258 5.498.142.184.36.29.592.29.23 0 .449-.107.591-.291 1.002-1.299 3.044-3.945 4.243-5.498z"/>
                     </svg>
                 </button>
             </div>
             {/* Options Wrapper */}
-            
             {(searchTerm && showOptions && options.length > 0) && (
                 <div className="absolute z-10 top-full mt-1 border-2 border-solid border-gray-400 overflow-hidden rounded-xl w-full bg-gray-400 max-h-60">
                     {/* Internal wrapper so the scrollbar doesn't affect the border and the options don't come over rounded corners */}
@@ -145,9 +141,6 @@ const MultiSelector = <T extends BaseOption,>({
                     </ul>
                 </div>
             )}
-
-            
-            
         </div>
     )
 }
